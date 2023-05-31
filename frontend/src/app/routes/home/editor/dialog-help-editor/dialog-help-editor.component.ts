a396c6c368c14c9cf6b31c6dd2bd3d4238950641
benediktreuter <reuter@hdm-stuttgart.de>
2023-05-31 12:46:30 +0200

    add some shortcuts

--- frontend/src/app/routes/home/editor/dialog-help-editor/dialog-help-editor.component.ts
+++ frontend/src/app/routes/home/editor/dialog-help-editor/dialog-help-editor.component.ts
@@ -34,6 +34,16 @@ export class DialogHelpEditorComponent {
         key: ['Command', 'Shift', '/'],
         command: $localize`:@@editorHelpDialogShortcutCommandToggleComment:Toggle Comment`,
       },
+      {
+        key: ['Backspace'],
+        command:
+          'Wenn der Cursor am Anfang des Textes ist, wird die Caption gelöscht. Ansonsten wird wie gewohnt das letzte Zeichen gelöscht.',
+      },
+      {
+        key: ['Enter'],
+        command: 'Verschiebe den Text ab dem Cursor in eine neue Caption.',
+      },
+      { key: ['Command', 'Enter'], command: 'Füge eine neue Zeile hinzu.' },
     ];
 
     const shortcutsWindows: Shortcut[] = [
@@ -49,6 +59,21 @@ export class DialogHelpEditorComponent {
         key: ['STRG', 'Shift', '/'],
         command: $localize`:@@editorHelpDialogShortcutCommandToggleComment:Toggle Comment`,
       },
+      {
+        key: ['Backspace'],
+        command:
+          'Wenn der Cursor am Anfang des Textes ist, wird die Caption gelöscht. Ansonsten wird wie gewohnt das letzte Zeichen gelöscht.',
+      },
+      {
+        key: ['Enter'],
+        command:
+          'Verschiebe den Text ab dem Cursor in die nächste Caption oder in eine neue Caption (wenn die aktuelle caption > 6 sekunden ist, wird sie gesplitted).',
+      },
+      {
+        key: ['SHIFT', 'Enter'],
+        command: 'Verschiebe den Text ab dem Cursor in eine neue Caption.',
+      },
+      { key: ['STRG', 'Enter'], command: 'Füge eine neue Zeile hinzu.' },
     ];
 
     this.shortcuts = navigator.userAgent.includes('Mac')
