81e6b887aa6d866e012a008ed8fa248aa7c92c31
benediktreuter <reuter@hdm-stuttgart.de>
2024-02-08 11:48:56 +0100

    cleanups

--- frontend/src/app/store/effects/config.effects.ts
+++ frontend/src/app/store/effects/config.effects.ts
@@ -24,7 +24,7 @@ export class ConfigEffects {
     this.actions$.pipe(
       ofType(configActions.fetch),
       mergeMap(
-        (action) =>
+        () =>
           this.api.getConfig().pipe(
             map((resData) => {
               return configActions.fetchSuccess({ configEntity: resData });
