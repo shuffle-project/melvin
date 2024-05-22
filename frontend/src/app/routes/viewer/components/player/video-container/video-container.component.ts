63d10f3907bd0847d45ba03f9d90c589f3cf9dd2
BeneReu <benedikt.reuter@posteo.de>
2024-05-22 08:06:19 +0200

    log audio offset

--- frontend/src/app/routes/viewer/components/player/video-container/video-container.component.ts
+++ frontend/src/app/routes/viewer/components/player/video-container/video-container.component.ts
@@ -12,7 +12,7 @@ import { Store } from '@ngrx/store';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PushPipe } from '@ngrx/component';
-import { Subject, takeUntil, tap } from 'rxjs';
+import { Subject, takeUntil, tap, throttleTime } from 'rxjs';
 import * as viewerActions from '../../../../../store/actions/viewer.actions';
 import { AppState } from '../../../../../store/app.state';
 import * as viewerSelector from '../../../../../store/selectors/viewer.selector';
@@ -181,43 +181,43 @@ export class VideoContainerComponent implements OnDestroy, OnChanges {
       )
       .subscribe();
 
-    // // sync to audio
-    // this.viewerService.currentTime$
-    //   .pipe(
-    //     takeUntil(this.destroy$$),
-    //     throttleTime(5000),
-    //     tap((audioCurrentTime: number) => {
-    //       // return if viewerVideo duration is already reached
-    //       if (this.viewerVideoElement.duration < audioCurrentTime) return;
+    // sync to audio
+    this.viewerService.currentTime$
+      .pipe(
+        takeUntil(this.destroy$$),
+        throttleTime(5000),
+        tap((audioCurrentTime: number) => {
+          // return if viewerVideo duration is already reached
+          if (this.viewerVideoElement.duration < audioCurrentTime) return;
 
-    //       const audioTime = audioCurrentTime;
-    //       const videoTime = this.viewerVideoElement.currentTime;
-    //       const audioOffsetMS = (audioTime - videoTime) * 1000;
+          const audioTime = audioCurrentTime;
+          const videoTime = this.viewerVideoElement.currentTime;
+          const audioOffsetMS = (audioTime - videoTime) * 1000;
 
-    //       /**
-    //        * https://en.wikipedia.org/wiki/Audio-to-video_synchronization#:~:text=The%20EBU%20Recommendation%20R37%20%22The,5%20ms%20and%20%2D15%20ms.
-    //        * The EBU Recommendation R37 "The relative timing of the sound and vision components of a television signal"
-    //        * states that end-to-end audio/video sync should be within +40 ms and -60 ms (audio before/after video, respectively)
-    //        *  and that each stage should be within +5 ms and -15 ms.
-    //        *
-    //        * ATSC IS-191: -45ms to 15ms
-    //        * EBU R37-2007: -60ms to 40ms
-    //        * ITU BT.1359-1: -125ms to 45ms
-    //        * ITU BR.265-9: -22ms to 22ms
-    //        */
+          /**
+           * https://en.wikipedia.org/wiki/Audio-to-video_synchronization#:~:text=The%20EBU%20Recommendation%20R37%20%22The,5%20ms%20and%20%2D15%20ms.
+           * The EBU Recommendation R37 "The relative timing of the sound and vision components of a television signal"
+           * states that end-to-end audio/video sync should be within +40 ms and -60 ms (audio before/after video, respectively)
+           *  and that each stage should be within +5 ms and -15 ms.
+           *
+           * ATSC IS-191: -45ms to 15ms
+           * EBU R37-2007: -60ms to 40ms
+           * ITU BT.1359-1: -125ms to 45ms
+           * ITU BR.265-9: -22ms to 22ms
+           */
 
-    //       const msBefore = 40;
-    //       const msAfter = -60;
+          const msBefore = 40;
+          const msAfter = -60;
 
-    //       if (audioOffsetMS > msBefore || audioOffsetMS < msAfter) {
-    //         console.log('resync video  ', audioOffsetMS);
+          if (audioOffsetMS > msBefore || audioOffsetMS < msAfter) {
+            console.log('resync video  ', audioOffsetMS);
 
-    //         // TODO ready state video
-    //         this.viewerVideoElement.currentTime = audioCurrentTime;
-    //       }
-    //     })
-    //   )
-    //   .subscribe();
+            // TODO ready state video
+            // this.viewerVideoElement.currentTime = audioCurrentTime;
+          }
+        })
+      )
+      .subscribe();
   }
 
   private setCurrentState(videoPlayer: HTMLVideoElement) {
