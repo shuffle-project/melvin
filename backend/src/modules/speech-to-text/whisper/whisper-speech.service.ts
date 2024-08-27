ce1cfda01fc4e47a02a2f8b3cf65ae0f6d3da07c
Benes <benedikt.reuter@posteo.de>
2024-08-27 13:23:51 +0200

    get languages from whisper server config

--- backend/src/modules/speech-to-text/whisper/whisper-speech.service.ts
+++ backend/src/modules/speech-to-text/whisper/whisper-speech.service.ts
@@ -16,7 +16,11 @@ import {
   TranscriptEntity,
   WordEntity,
 } from '../speech-to-text.interfaces';
-import { WhiTranscribeDto, WhiTranscriptEntity } from './whisper.interfaces';
+import {
+  WhiInformation,
+  WhiTranscribeDto,
+  WhiTranscriptEntity,
+} from './whisper.interfaces';
 
 // npm rebuild bcrypt --build-from-source
 
@@ -46,134 +50,39 @@ export class WhisperSpeechService implements ISpeechToTextService {
     if (!this.whisperConfig) {
       return null;
     }
+    try {
+      const response = await lastValueFrom(
+        this.httpService
+          .get<WhiInformation>(`${this.host}`, {
+            headers: {
+              Authorization: this.apikey,
+            },
+          })
+          .pipe(
+            map((res: AxiosResponse<WhiInformation>) => {
+              return res.data;
+            }),
+            catchError((error: AxiosError) => {
+              if (error?.response?.status) {
+                throw new HttpException(
+                  error.response.data,
+                  error.response.status,
+                );
+              } else {
+                throw new HttpException('unknown error', 500);
+              }
+            }),
+          ),
+      );
 
-    return new Promise((resolve) => {
-      resolve([
-        // { code: 'en', name: 'English' },
-        // { code: 'de', name: 'German' },
-        // { code: 'fr', name: 'French' },
-        // { code: 'es', name: 'Spanish' },
-        // { code: 'it', name: 'Italian' },
-
-        ...[
-          'af',
-          'am',
-          'ar',
-          'as',
-          'az',
-          'ba',
-          'be',
-          'bg',
-          'bn',
-          'bo',
-          'br',
-          'bs',
-          'ca',
-          'cs',
-          'cy',
-          'da',
-          'de',
-          'el',
-          'en',
-          'es',
-          'et',
-          'eu',
-          'fa',
-          'fi',
-          'fo',
-          'fr',
-          'gl',
-          'gu',
-          'ha',
-          'haw',
-          'he',
-          'hi',
-          'hr',
-          'ht',
-          'hu',
-          'hy',
-          'id',
-          'is',
-          'it',
-          'ja',
-          'jw',
-          'ka',
-          'kk',
-          'km',
-          'kn',
-          'ko',
-          'la',
-          'lb',
-          'ln',
-          'lo',
-          'lt',
-          'lv',
-          'mg',
-          'mi',
-          'mk',
-          'ml',
-          'mn',
-          'mr',
-          'ms',
-          'mt',
-          'my',
-          'ne',
-          'nl',
-          'nn',
-          'no',
-          'oc',
-          'pa',
-          'pl',
-          'ps',
-          'pt',
-          'ro',
-          'ru',
-          'sa',
-          'sd',
-          'si',
-          'sk',
-          'sl',
-          'sn',
-          'so',
-          'sq',
-          'sr',
-          'su',
-          'sv',
-          'sw',
-          'ta',
-          'te',
-          'tg',
-          'th',
-          'tk',
-          'tl',
-          'tr',
-          'tt',
-          'uk',
-          'ur',
-          'uz',
-          'vi',
-          'yi',
-          'yo',
-          'zh',
-        ].map((code) => ({ code, name: code })),
-
-        // von whisper -h
-        //
-        // af,am,ar,as,az,ba,be,bg,bn,bo,br,bs,ca,cs,cy,da,de,el,en,es,et,eu,fa,fi,fo,fr,gl,
-        // gu,ha,haw,he,hi,hr,ht,hu,hy,id,is,it,ja,jw,ka,kk,km,kn,ko,la,lb,ln,lo,lt,lv,mg,mi,
-        // mk,ml,mn,mr,ms,mt,my,ne,nl,nn,no,oc,pa,pl,ps,pt,ro,ru,sa,sd,si,sk,sl,sn,so,sq,sr,su,
-        // sv,sw,ta,te,tg,th,tk,tl,tr,tt,uk,ur,uz,vi,yi,yo,zh,
-        // Afrikaans,Albanian,Amharic,Arabic,Armenian,Assamese,Azerbaijani,Bashkir,Basque,Belarusian,
-        // Bengali,Bosnian,Breton,Bulgarian,Burmese,Castilian,Catalan,Chinese,Croatian,Czech,Danish,Dutch,English,
-        // Estonian,Faroese,Finnish,Flemish,French,Galician,Georgian,German,Greek,Gujarati,Haitian,Haitian Creole,Hausa,
-        // Hawaiian,Hebrew,Hindi,Hungarian,Icelandic,Indonesian,Italian,Japanese,Javanese,Kannada,Kazakh,Khmer,Korean,Lao,
-        // Latin,Latvian,Letzeburgesch,Lingala,Lithuanian,Luxembourgish,Macedonian,Malagasy,Malay,Malayalam,Maltese,Maori,
-        // Marathi,Moldavian,Moldovan,Mongolian,Myanmar,Nepali,Norwegian,Nynorsk,Occitan,Panjabi,Pashto,Persian,Polish,
-        // Portuguese,Punjabi,Pushto,Romanian,Russian,Sanskrit,Serbian,Shona,Sindhi,Sinhala,Sinhalese,Slovak,Slovenian,
-        // Somali,Spanish,Sundanese,Swahili,Swedish,Tagalog,Tajik,Tamil,Tatar,Telugu,Thai,Tibetan,Turkish,Turkmen,Ukrainian,
-        // Urdu,Uzbek,Valencian,Vietnamese,Welsh,Yiddish,Yoruba
-      ]);
-    });
+      return response.supported_language_codes.map((lang) => ({
+        code: lang,
+        name: lang,
+      }));
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 
   async run(project: Project, audio: Audio): Promise<TranscriptEntity> {
@@ -255,7 +164,7 @@ export class WhisperSpeechService implements ISpeechToTextService {
     const formData = new FormData();
     formData.append('file', file, audio._id.toString() + '.' + audio.extension);
     const settings: WhiTranscribeDto = {
-      language: project.language,
+      language: this._getLanguage(project.language),
       condition_on_previous_text: false,
     };
     formData.append('settings', JSON.stringify(settings));
