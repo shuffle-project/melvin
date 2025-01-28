07c906ba536b2ccef50f308b3d698b255a67a54b
Benes <benedikt.reuter@posteo.de>
2025-01-28 09:53:36 +0100

    add url in project media entity for base file

--- frontend/src/app/routes/home/editor/service/media/media.service.ts
+++ frontend/src/app/routes/home/editor/service/media/media.service.ts
@@ -81,14 +81,14 @@ export class MediaService {
     )
       .pipe(
         // debounceTime(0),
-        tap((event) => {
+        tap(() => {
           // HAVE_NOTHING	0	No information is available about the media resource.
           // HAVE_METADATA	1	Enough of the media resource has been retrieved that the metadata attributes are initialized. Seeking will no longer raise an exception.
           // HAVE_CURRENT_DATA	2	Data is available for the current playback position, but not enough to actually play more than one frame.
           // HAVE_FUTURE_DATA	3	Data for the current playback position as well as for at least a little bit of time into the future is available (in other words, at least two frames of video, for example).
           // HAVE_ENOUGH_DATA	4	Enough data is available—and the download rate is high enough—that the media can be
 
-          console.log(media.readyState, id, event.type);
+          // console.log(media.readyState, id, event.type);
           if (media.readyState > 3) {
             this.store.dispatch(editorActions.eMediaLoaded({ id }));
           } else {
