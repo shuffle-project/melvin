99063cef5498822fc2d5c6aa7e6838d2b51e8d56
Niklas Egger <egger@hdm-stuttgart.de>
2025-04-08 10:46:14 +0200

    💄 Revise Footer

--- frontend/src/app/routes/landing/landing.routes.ts
+++ frontend/src/app/routes/landing/landing.routes.ts
@@ -64,7 +64,7 @@ export const LandingRoutes: Routes = [
       {
         path: 'guide',
         component: GuideComponent,
-        title: $localize`:@@GuidePageTitle:Guide - Melvin`,
+        title: $localize`:@@GuidePageTitle:Didactic Guide - Melvin`,
       },
       //   {
       //     path: '**',
