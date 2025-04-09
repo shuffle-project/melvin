846e5083839c54c1fc2a754e1f5811ac348b6fd1
Benes <benedikt.reuter@posteo.de>
2025-04-09 12:34:29 +0200

    ✨ active task service - cleanup temp dir

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from '../db/db.module';
 import { LoggerModule } from '../logger/logger.module';
 import { PathModule } from '../path/path.module';
 import { TasksService } from './tasks.service';
 
 @Module({
   providers: [TasksService],
-  imports: [LoggerModule, PathModule],
+  imports: [LoggerModule, PathModule, DbModule],
   exports: [TasksService],
 })
 export class TasksModule {}
--- backend/src/modules/tasks/tasks.service.ts
+++ backend/src/modules/tasks/tasks.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
-import { readdir, stat } from 'fs/promises';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
+import { readdir, rmdir, stat } from 'fs/promises';
 import { join } from 'path';
 import { validate } from 'uuid';
+import { DbService } from '../db/db.service';
 import { CustomLogger } from '../logger/logger.service';
 import { PathService } from '../path/path.service';
 
 @Injectable()
 export class TasksService {
-  constructor(private logger: CustomLogger, private pathService: PathService) {
+  constructor(
+    private logger: CustomLogger,
+    private pathService: PathService,
+    private db: DbService,
+  ) {
     this.logger.setContext(this.constructor.name);
   }
 
-  // @Cron('30 * * * * *') // every minute in the 30th second -> for testing
-  @Cron('0 4 * * *') // every day at 4:00 AM
+  // @Cron(CronExpression.EVERY_HOUR) // testing
+  @Cron(CronExpression.EVERY_DAY_AT_4AM) // every day at 4:00 AM
   async cleanupTempDirectory() {
     this.logger.log('Cleanup temp directory via Cronjob');
     const rootTempDir = this.pathService.getRootTempDirectory();
@@ -23,16 +29,15 @@ export class TasksService {
       const dirPath = join(rootTempDir, dir);
       const tempDirStat = await stat(dirPath);
 
+      // if temp folder is a valid uuid
       if (validate(dir)) {
         const ageInMs = new Date().valueOf() - tempDirStat.birthtime.valueOf();
         const ageInDays = ageInMs / 1000 / 60 / 60 / 24;
+        // remove everything older than 7 days
         if (ageInDays > 7) {
-          console.log('====' + dir);
-          console.log('age in days:', ageInDays);
-
-          //   await rmdir(dirPath, { recursive: true });
+          await rmdir(dirPath, { recursive: true });
           this.logger.info(
-            'removed temp dir(' + Math.round(ageInDays) + '): ' + dirPath,
+            'removed temp dir(' + Math.round(ageInDays) + 'd old): ' + dirPath,
           );
         }
       }
