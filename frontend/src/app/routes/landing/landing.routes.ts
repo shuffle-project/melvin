495430c3695313bb0cf3cc305f4aa1bc88fb3bb7
Niklas Egger <egger@hdm-stuttgart.de>
2025-05-16 11:25:17 +0200

    🌐 Change Didactic Guide Label to Best Practice

--- frontend/src/app/routes/landing/landing.routes.ts
+++ frontend/src/app/routes/landing/landing.routes.ts
@@ -64,7 +64,7 @@ export const LandingRoutes: Routes = [
       {
         path: 'guide',
         component: GuideComponent,
-        title: $localize`:@@GuidePageTitle:Didactic Guide - Melvin`,
+        title: $localize`:@@GuidePageTitle:Best Practice - Melvin`,
       },
       //   {
       //     path: '**',
