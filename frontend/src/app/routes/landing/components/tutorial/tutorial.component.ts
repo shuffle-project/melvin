2c53d86d09e1b29ff2d425d5a159b286ca7ca15e
Niklas Egger <egger@hdm-stuttgart.de>
2025-05-22 14:02:22 +0200

    🚧 Implement English Text-Version

--- frontend/src/app/routes/landing/components/tutorial/tutorial.component.ts
+++ frontend/src/app/routes/landing/components/tutorial/tutorial.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { TutorialListComponent } from './tutorial-list/tutorial-list/tutorial-list.component';
-import { RECORD_TUTORIAL } from './tutorial.constant';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tutorial',
-  imports: [MatTabsModule, TutorialListComponent],
+  imports: [MatTabsModule, MatIconModule, RouterLink],
   templateUrl: './tutorial.component.html',
   styleUrl: './tutorial.component.scss',
 })
 export class TutorialComponent {
-  recordTutorial = RECORD_TUTORIAL;
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // if (!this.route.snapshot.fragment) {
+    //   const element = document.getElementById('tutorial-heading');
+    //   if (element) {
+    //     element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    //   }
+    // }
+
+    this.router.events.subscribe(() => {
+      const fragment = this.route.snapshot.fragment;
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+      }
+    });
+  }
 }
