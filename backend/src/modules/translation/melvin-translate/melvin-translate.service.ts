9e9be43a6d6296664a30a67d4ca941021dfdbc65
Benes <benedikt.reuter@posteo.de>
2025-07-02 10:21:22 +0200

    ⚰️ remove dead code

--- backend/src/modules/translation/melvin-translate/melvin-translate.service.ts
+++ backend/src/modules/translation/melvin-translate/melvin-translate.service.ts
@@ -6,7 +6,6 @@ import { LanguageShort } from 'src/app.interfaces';
 import { WhisperConfig } from 'src/config/config.interface';
 import { Project } from 'src/modules/db/schemas/project.schema';
 import { CustomLogger } from 'src/modules/logger/logger.service';
-import { MelvinAsrResultEntity } from 'src/modules/melvin-asr-api/melvin-asr-api.interfaces';
 import { MelvinAsrApiService } from 'src/modules/melvin-asr-api/melvin-asr-api.service';
 import { ProcessMelvinAsrJob } from 'src/processors/melvin-asr.processor';
 import { TranscriptionEntity } from 'src/resources/transcription/entities/transcription.entity';
@@ -77,33 +76,5 @@ export class MelvinTranslateService {
     );
 
     // return this._fetchResult(translate.id);
-  }
-
-  // copy from whisper speech service
-  // TODO refactor to queue
-  private async _fetchResult(id: string) {
-    const transcriptEntity: MelvinAsrResultEntity | null = await new Promise(
-      (resolve) => {
-        const interval = setInterval(async () => {
-          const job = await this.melvinAsrApiService.getJob(id);
-          if (job.status === 'completed' || job.status === 'failed') {
-            clearInterval(interval);
-            if (job.status === 'failed') {
-              resolve(null);
-            } else {
-              const result = await this.melvinAsrApiService.getJobResult(id);
-              resolve(result);
-            }
-          }
-        }, 10000);
-      },
-    );
-
-    if (!transcriptEntity) {
-      // TODO refactor in queue
-      throw new Error('Internal Error in MelvinASR');
-    }
-    const words = this.melvinAsrApiService.toWords(transcriptEntity, false);
-    return { words };
   }
 }
