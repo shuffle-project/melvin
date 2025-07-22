80c3b76365edaf6f18f1462882123d9edbc9122d
Niklas Egger <egger@hdm-stuttgart.de>
2025-07-22 10:13:16 +0200

    🚧 Trying to Fix New Approach

--- frontend/nginx.sh
+++ frontend/nginx.sh
@@ -4,10 +4,10 @@ find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,FRONTEND_BASE_URL,'
 cat <<EOF > /usr/share/nginx/html/assets/env.js
 window.env = {
     MELVIN_IMPRINT_URL: "${MELVIN_IMPRINT_URL}",
-    MELVIN_DE_IMPRINT_URL: "${MELVIN_DE_IMPRINT_URL}",
     MELVIN_PRIVACY_URL: "${MELVIN_PRIVACY_URL}",
+    MELVIN_DE_IMPRINT_URL: "${MELVIN_DE_IMPRINT_URL}",
     MELVIN_DE_PRIVACY_URL: "${MELVIN_DE_PRIVACY_URL}",
 };
 EOF
 
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"
