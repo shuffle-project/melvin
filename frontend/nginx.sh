a3957b74edcdfb7b968ab887cb43c002f13ffa72
Niklas Egger <egger@hdm-stuttgart.de>
2025-07-22 11:20:56 +0200

    🚧 New Fix Approach

--- frontend/nginx.sh
+++ frontend/nginx.sh
@@ -1,3 +1,5 @@
+ #!/bin/sh
+
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,BACKEND_BASE_URL,'"$BACKEND_BASE_URL"',g' {} \;
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,FRONTEND_BASE_URL,'"$FRONTEND_BASE_URL"',g' {} \;
 
