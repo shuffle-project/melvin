5d2d71c985943bf317ba0a1c472a725b25f3d08c
Niklas Egger <egger@hdm-stuttgart.de>
2025-07-24 14:37:45 +0200

    Refactor sitemap component to use dynamic URLs for privacy and imprint links

--- frontend/src/app/routes/landing/components/sitemap/sitemap.component.ts
+++ frontend/src/app/routes/landing/components/sitemap/sitemap.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FeatureEnabledPipe } from 'src/app/pipes/feature-enabled-pipe/feature-enabled.pipe';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
-    selector: 'app-sitemap',
-    imports: [RouterLink, FeatureEnabledPipe],
-    templateUrl: './sitemap.component.html',
-    styleUrl: './sitemap.component.scss'
+  selector: 'app-sitemap',
+  imports: [RouterLink, FeatureEnabledPipe],
+  templateUrl: './sitemap.component.html',
+  styleUrl: './sitemap.component.scss',
 })
 export class SitemapComponent {
   locale = $localize.locale;
+
+  imprintUrl = this.configService.getImprintUrl();
+  privacyUrl = this.configService.getPrivacyUrl();
+
+  constructor(private configService: ConfigService) {}
 }
