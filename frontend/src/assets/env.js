89994cea6b84aa2011bd4ed55a0c39fb84660bb5
Niklas Egger <egger@hdm-stuttgart.de>
2025-07-24 14:28:29 +0200

    Update footer links to use environment variables for Privacy and Imprint URLs

--- frontend/src/assets/env.js
+++ frontend/src/assets/env.js
@@ -2,8 +2,8 @@
 // Check the Docker-Compose file for the env variables.
 
 window.env = {
-  MELVIN_IMPRINT_URL: '',
-  MELVIN_PRIVACY_URL: '',
+  MELVIN_IMPRINT_URL: '/imprint',
+  MELVIN_PRIVACY_URL: '/privacy',
   MELVIN_FRONTEND_BASE_URL: 'http://localhost:4200',
   MELVIN_BACKEND_BASE_URL: 'http://localhost:3000',
 };
