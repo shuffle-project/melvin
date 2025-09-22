f242900974830954859fd79143af2b34590209f0
Benes <benedikt.reuter@posteo.de>
2025-09-22 07:34:41 +0200

    ✨ add mail setup

--- backend/src/modules/mail/mail.service.ts
+++ backend/src/modules/mail/mail.service.ts
@@ -1,12 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createTransport, Transporter } from 'nodemailer';
+import { EmailConfig } from 'src/config/config.interface';
 import { LeanProjectDocument } from '../db/schemas/project.schema';
 import { LeanUserDocument } from '../db/schemas/user.schema';
 import { CustomLogger } from '../logger/logger.service';
 
 @Injectable()
 export class MailService {
-  constructor(private logger: CustomLogger) {
+  emailConfig: EmailConfig;
+  transporter: Transporter;
+
+  constructor(
+    private logger: CustomLogger,
+    private configService: ConfigService,
+  ) {
     this.logger.setContext(this.constructor.name);
+
+    this.emailConfig = this.configService.get<EmailConfig>('email');
+  }
+
+  async onApplicationBootstrap(): Promise<void> {
+    console.log('onapplicationBootstrap mail');
+    console.log(this.emailConfig);
+    if (this.emailConfig) {
+      this.transporter = createTransport({
+        host: this.emailConfig.smtpServer,
+        port: this.emailConfig.smtpPort,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: this.emailConfig.user,
+          pass: this.emailConfig.password,
+        },
+      });
+      this.transporter.verify().then((v) => {
+        console.log('verified:', v);
+      });
+
+      // this.transporter
+      //   .sendMail({
+      //     from: `${this.emailConfig.mailFromName} <${this.emailConfig.mailFrom}>`,
+      //     to: 'benedikt.reuter@posteo.de',
+      //     subject: 'Test Email from melvin',
+      //     text: ' This is a test email to verify the SMTP configuration.',
+      //   })
+      //   .then((info) => {
+      //     console.log(info);
+      //   })
+      //   .catch((err) => {
+      //     console.error(err);
+      //   });
+    }
+  }
+
+  async _sendMail(subject: string, text: string, html: string) {
+    return await this.transporter.sendMail({
+      from: `Melvin Admin <${this.emailConfig.mailFrom}>`,
+      // to: `Recipient <${this.emailConfig.mailFrom}>`,
+      to: `Recipient <benedikt.reuter@posteo.de>`,
+      subject,
+      text,
+      html,
+    });
   }
 
   async sendInviteEmail(
--- backend/src/resources/admin/admin.module.ts
+++ backend/src/resources/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from 'src/modules/db/db.module';
+import { MailModule } from 'src/modules/mail/mail.module';
 import { PathModule } from 'src/modules/path/path.module';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
@@ -8,7 +9,7 @@ import { AdminService } from './admin.service';
 
 @Module({
   controllers: [AdminController],
-  imports: [DbModule, PathModule, AuthModule, UserModule],
+  imports: [DbModule, PathModule, AuthModule, UserModule, MailModule],
   providers: [AdminService],
 })
 export class AdminModule {}
